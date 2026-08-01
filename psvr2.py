d53e75d55bb44b60234567b6630755905b93a4ed
Cupri <epicfellow23@gmail.com>
2026-08-01 00:28:54 -0400

    Fix linux, and add wayvr launch argument.
    New update allows users to launch wayvr instead of steamvr, fixes extras feature, and improves linux support.

--- psvr2.py
+++ psvr2.py
@@ -2,15 +2,20 @@ import subprocess
 import webbrowser
 import os
 from sys import platform
+from sys import argv
 # imports above
 base = os.path.dirname(os.path.abspath(__file__))
+wayvr = False
+if len(argv) >= 2:
+    if argv[1] == "wayvr":
+        wayvr=True
+        print("Using wayvr!")
 print("Initiating jailbreak...") # start vr2jb
 try:
     print("Detected platform: " + platform)
     if platform == "win32":
         ran = subprocess.run("vr2jb/vr2jb.exe")
-    else:
-        ran = subprocess.run("vr2jb/vr2jb")
+    else: ran = subprocess.run("vr2jb/vr2jb")
     if ran.returncode != 0:
         print("jailbreak failed! check that a folder named \"vr2jb\" exists and is near the script, and contains vr2jb")
         if platform != "win32": print("Make sure to run \"chmod +x ./*\" in the vr2jb folder.")
@@ -19,14 +24,25 @@ try:
 except FileNotFoundError:
     print("No jailbreak folder found! Skipping jailbreak...")
 print("initializing SteamVR") # start steamvr
-print("Starting SteamVR...")
-webbrowser.open("steam://rungameid/250820")
+if wayvr:
+    print("Starting WayVR...")
+    ran = subprocess.run("wayvr", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if ran != 0: print("Error starting WayVR")
+    else: print("WayVR started successfully!")
+else:
+    print("Starting SteamVR")
+    webbrowser.open("steam://rungameid/250820")
 try:
     extras = os.listdir(os.path.join(base, "extras"))
     if len(extras) != 0:
         print("Extras found!")
-        for extra in extras:
-            os.startfile(os.path.join(base, "extras", extra))
+        if platform != "win32":
+            for extra in extras:
+                if extra.endswith(".sh"): subprocess.run(["bash", "extras/" + extra])
+                else: subprocess.run("extras/" + extra)
+        else:
+            for extra in extras:
+                os.startfile(os.path.join(base, "extras", extra))
     else:
         print("Extras not found.")
 except FileNotFoundError:
